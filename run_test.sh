1739b820838c3d230a87bec66de46149a45c282b
Byron Lee <byron.lee@grindr.com>
2020-04-27 20:56:16 +0800

    Update testing script

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,5 @@
 mkdir output
-# make
 policy=("FIFO" "RR" "SJF" "PSJF" )
-
 for ((i=0; i < ${#policy[@]}; i++)); do
     for (( j = 1; j <= 5; j++ )); do
         filename=${policy[$i]}_${j}
@@ -10,5 +8,7 @@ for ((i=0; i < ${#policy[@]}; i++)); do
         sudo ./main < "OS_PJ1_Test/$filename.txt" > "output/${filename}_stdout.txt"
         dmesg | grep Project1 > "output/${filename}_dmesg.txt"
     done
-
 done
+sudo dmesg -c
+sudo ./main < "OS_PJ1_Test/TIME_MEASUREMENT.txt" > "output/TIME_MEASUREMENT_stdout.txt"
+dmesg | grep Project1 > "output/TIME_MEASUREMENT_dmesg.txt"
