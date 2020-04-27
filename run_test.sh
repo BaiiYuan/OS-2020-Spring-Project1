9f83b0a0963ca8e8c5bd03b92ce8b685cd962822
Byron Lee <byron.lee@grindr.com>
2020-04-27 16:06:49 +0800

    Add test script

---
+++ run_test.sh
@@ -0,0 +1,14 @@
+mkdir output
+# make
+policy=("FIFO" "RR" "SJF" "PSJF" )
+
+for ((i=0; i < ${#policy[@]}; i++)); do
+    for (( j = 1; j <= 5; j++ )); do
+        filename=${policy[$i]}_${j}
+        echo "Process OS_PJ1_Test/$filename.txt ..."
+        sudo dmesg -c
+        sudo ./main < "OS_PJ1_Test/$filename.txt" > "output/${filename}_stdout.txt"
+        dmesg | grep Project1 > "output/${filename}_dmesg.txt"
+    done
+
+done
